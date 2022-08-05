15b7a3eba4d2cb7c425a3a053aaacee15ed49f2c
Sonal Kumar Sinha <sinha.sonal.kumar@gmail.com>
2022-08-05 12:34:09 -0600

    feat: for props

--- src/main/java/com/sonal/skaffold/demo/msvc1/controller/HelloMSvc1Controller.java
+++ src/main/java/com/sonal/skaffold/demo/msvc1/controller/HelloMSvc1Controller.java
@@ -1,17 +1,22 @@
 package com.sonal.skaffold.demo.msvc1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sonal.skaffold.demo.msvc1.connfig.GreetingsConfig;
 
 import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloMSvc1Controller {
 
+	@Autowired
+	private GreetingsConfig greetingsConfig;
 	
 	@GetMapping
 	public Mono<String> helloMSvc1() {
 		
-		return Mono.just("Micro Service 1 wokring with Skaffold and Helm Nicely together.........###");
+		return Mono.just(greetingsConfig.getGreetingsMessage() +  " Micro Service 1 wokring with Skaffold and Helm Nicely together.........###");
 	}
 }
